4510ac7fae17b53a72d83cffcadcb733f421a1aa
ippachi <ippachi1018@gmail.com>
2021-01-17 15:46:26 +0900

    Part 1, Chapter 8

--- money.rb
+++ money.rb
@@ -4,6 +4,14 @@ module Money
 
     protected attr_reader :amount
 
+    def self.dollar(amount)
+      Dollar.new(amount)
+    end
+
+    def self.franc(amount)
+      Franc.new(amount)
+    end
+
     def <=>(other)
       return -1 if self.class != other.class
 
